ec3f9df68073474dc26cbf4d19a9ca6c98739d5c
vovaphp <sahno023@gmail.com>
2022-10-07 16:02:20 +0300

    autoloader

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo 'work';
+spl_autoload_register(function ($className) {
+    $classFile = 'app/' . $className . '.php';
+    if (file_exists($classFile)) {
+        include_once $classFile;
+        return true;
+    }
+    return false;
+});
